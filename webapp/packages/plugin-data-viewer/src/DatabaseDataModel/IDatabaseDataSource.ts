586d4154ab96c3655c0109171fdfbfaab7ce03c4
Sychev Andrey <44414066+SychevAndrey@users.noreply.github.com>
2026-02-10 09:41:39 +0100

    dbeaver/pro#4999  Data editor icons to see pk details (#4040)
    * dbeaver/pro#4999 feat: add TableStatusIndicator component and update tooltips for key detection
    
    * dbeaver/pro#4999 feat: update TableStatusIndicator to handle read-only connection state and improve tooltips
    
    * dbeaver/pro#4999 feat: use new backend endpoint to provide info about identifier
    
    * dbeaver/pro#4999 refactor: simplify TableStatusIndicator by removing props and using context directly
    
    * dbeaver/pro#4999 refactor: move hasElementIdentifier to ResultSetDataSource
    
    * pr fixes
    
    * fixes indicator center position + removes duplicated translation
    
    * fixes contrast for color-status (JSON edit row + keys indicator for tables)
    
    ---------
    
    Co-authored-by: sergeyteleshev <iamsergeyteleshev@gmail.com>
    Co-authored-by: Daria Marutkina <125263541+dariamarutkina@users.noreply.github.com>

--- webapp/packages/plugin-data-viewer/src/DatabaseDataModel/IDatabaseDataSource.ts
+++ webapp/packages/plugin-data-viewer/src/DatabaseDataModel/IDatabaseDataSource.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2025 DBeaver Corp and others
+ * Copyright (C) 2020-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,6 @@ export interface IDatabaseDataSource<TOptions = unknown, TResult extends IDataba
   isOutdated: () => boolean;
   isLoadable: () => boolean;
   isReadonly: (resultIndex: number) => boolean;
-  hasElementIdentifier: (resultIndex: number) => boolean;
   isDataAvailable: (offset: number, count: number) => boolean;
   isLoading: () => boolean;
   isDisabled: (resultIndex?: number) => boolean;
